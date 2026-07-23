47e50f459c6720bff2fc7ca183a7fdf4392280c7
ahnjb <159648234+jeongbin-0509@users.noreply.github.com>
2026-07-23 12:22:05 +0900

    운지

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,4 +1,5 @@
 from routes.auth import register_auth_routes
+from routes.dashboard import register_dashboard_routes
 from routes.errors import register_error_handlers
 from routes.goal import register_goal_routes
 from routes.main import register_main_routes
@@ -9,5 +10,6 @@ def register_routes(app):
     register_auth_routes(app)
     register_main_routes(app)
     register_study_routes(app)
+    register_dashboard_routes(app)
     register_goal_routes(app)
     register_error_handlers(app)
